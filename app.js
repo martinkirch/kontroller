15ddea8979b9bbb832626e067ae1bd2434f831c1
Martin Kirchgessner <martin@mkir.ch>
2012-09-11 16:18:29 +0200

    aaaaand now we can loop

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	var parent = $('#players')
-	var player1 = new Player(parent, 'jingles/batterie.wav')
+	var player1 = new Player(parent, 'jingles/batterie.wav', true)
 	var player2 = new Player(parent, 'jingles/coucou.wav')
 });
--- player.js
+++ player.js
@@ -2,10 +2,14 @@
  * Class tied to a "Play" button - yep, the big squares
  * @param parent HTML element
  * @param path to the sound (the "src" attribute)
+ * @param (optional) should we play it as a loop ?
  */
-function Player(parent, path) {
-	this.container = $('<div>').addClass('player').text(path)
-	this.player = $('<audio>').attr({src:path, preload:'auto'}).appendTo(this.container)
+function Player(parent, path, loop) {
+	this.container = $('<div>').addClass('player');
+	this.label = $('<p>').text(path).appendTo(this.container);
+	this.loop = $('<span>').addClass('btnLoop').text('⟳') // aka UTF8's CLOCKWISE GAPPED CIRCLE ARROW FTW
+		.appendTo(this.container);
+	this.player = $('<audio>').attr({src:path, preload:'auto'}).appendTo(this.container);
 	
 	var self = this;
 	
@@ -24,9 +28,30 @@ function Player(parent, path) {
 		}
 	})
 	
+	// End Of Playin' callback
 	this.player.bind('ended', function(e) {
 		self.container.removeClass('playing');	
 	});
+	
+	this.looping = false;
+	
+	// Loop trigger
+	this.loop.click(function(e) {
+		e.stopPropagation();
+		
+		if (self.looping) {
+			self.container.removeClass('looping');
+			self.player.removeAttr('loop');
+		} else {
+			self.container.addClass('looping');
+			self.player.attr('loop','loop');
+		}
+		
+		self.looping = !self.looping;
+	})
+	
+	if (loop)
+		this.loop.click();
 	
 	this.container.appendTo($(parent))
 }
