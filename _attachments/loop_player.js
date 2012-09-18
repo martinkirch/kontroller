d9579c95a329250425afc8cbd80d9ebc06e47ed8
Martin Kirchgessner <martin@mkir.ch>
2012-09-19 00:22:05 +0200

    THEY SEE ME LOOPIN' - THEY HATIN'

--- _attachments/loop_player.js
+++ _attachments/loop_player.js
@@ -8,8 +8,8 @@ this stuff is worth it, you can buy me a beer in return.
 */
 
 /**
- * Wrap TWO <AUDIO> players for looping playback
- * The two-players trick allow seamless loops - inspired from Daniel Gagan's "HTML5 Audio Loops" 2010 article
+ * Wrap TWO <AUDIO> players for loops playback
+ * Manual looping allow seamless loops - inspired by Géza Szekeres' method (http://onoffline.hu/html5loop/), with an additional trick !
  * @param the src attribute
  * @param its enclosing jQuery element
  */
@@ -19,30 +19,51 @@ function LoopPlayer (src, container) {
 		.appendTo(container);
 	
 	var playing = false;
+	var timeout = false;
 	
+	// MANUAL LOOP FTW
+	// Because using the "loop" tag in a single element inserts a short delay between loops....
+	
+	// wait for both elems to be created, then play a first time silently (coz first play always has a delay)
+	setTimeout(function() {
+		elems[1].volume = 0;
+		elems[1].play();
+		elems[0].volume = 0;
+		elems[0].play();
+		elems[1].currentTime = 0;
+		elems[1].pause();
+		elems[1].volume = 1;
+		elems[0].currentTime = 0;
+		elems[0].pause();
+		elems[0].volume = 1;
+		
+		// wait again (while previous "play" events are triggered), then bind the manual loop
+		setTimeout(function() {
+			elems.bind('play', function(e) {
+				playing = (playing+1) % 2;
+				timeout = setTimeout(function(){elems[playing].play()}, this.duration * 1000);
+				elems[playing].currentTime = 0;
+				elems[playing].pause();
+			});
+		}, 50);
+	}, 50);
+	
+	this.play = function() {
+		elems[0].play();
+	}
+	
+	this.stop = function() {
+		elems[0].currentTime = 0;
+		elems[0].pause();
+		elems[1].currentTime = 0;
+		elems[1].pause();
+		clearTimeout(timeout);
+		playing = false;
+		timeout = false;
+	}
+
 	this.remove = function() {
 		$(elems).remove();
 		container.trigger('ended');
 	}
-	
-	this.play = function() {
-		elems[0].play();
-		playing = 0;
-	}
-	
-	this.stop = function() {
-		elems[playing].pause();
-		elems[playing].currentTime = 0;
-		playing = false;
-	}
-	
-	// MANUAL LOOP FTW
-	// Because using the "loop" tag in a single element inserts a short delay between loops....
-	elems.bind('ended', function(e) {
-		var old = playing;
-		playing = (playing+1) % 2;
-		elems[playing].play();
-		elems[old].pause();
-		elems[old].currentTime = 0;
-	});
 };
